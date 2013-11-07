e8809ea7a2e977196e02d64ddf44bcaade8ce309
wrsulliv <wrsulliv@umass.edu>
2013-11-07 13:15:58 -0500

    Initial Commit

---
+++ MtGox Bitcoin Buyer/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MtGox_Bitcoin_Buyer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbl_Price = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.lbl_Interval = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Automation";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show Current Price";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Sell Bitcoins";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Buy Bitcoins";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 128);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(137, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Watch Price";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(156, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // lbl_Price
+            // 
+            this.lbl_Price.AutoSize = true;
+            this.lbl_Price.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Price.Location = new System.Drawing.Point(8, 202);
+            this.lbl_Price.Name = "lbl_Price";
+            this.lbl_Price.Size = new System.Drawing.Size(138, 24);
+            this.lbl_Price.TabIndex = 8;
+            this.lbl_Price.Text = "Unknown Price";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 157);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(137, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Send Email";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // lbl_Interval
+            // 
+            this.lbl_Interval.AutoSize = true;
+            this.lbl_Interval.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Interval.Location = new System.Drawing.Point(8, 235);
+            this.lbl_Interval.Name = "lbl_Interval";
+            this.lbl_Interval.Size = new System.Drawing.Size(154, 24);
+            this.lbl_Interval.TabIndex = 10;
+            this.lbl_Interval.Text = "Unknown Interval";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(155, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(137, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Run System!";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 293);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.lbl_Interval);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.lbl_Price);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lbl_Price;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label lbl_Interval;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ MtGox Bitcoin Buyer/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Net.Mail;
+
+namespace MtGox_Bitcoin_Buyer
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetLabelText_Del(String text);
+        delegate void SetIntervalLabelText_Del(Double minutes, bool up);
+        IWebDriver driver;
+        List<ExchangeData> exchangeData = new List<ExchangeData>();
+        Double tradeFee = 0.006; //  = Trade Fee Of 0.6%
+        Double numberOfBitcoins = 0.1;
+        int millisecondsPerDataPoint = 5000;
+        int dataPointsPerPeriod = 12;
+        Double minimumProfit = 0.2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            driver = new ChromeDriver();
+
+        }
+        private bool sellBitcoins(double number)
+        {
+            driver.Navigate().GoToUrl("http://mtgox.com/#sell");
+            if (driver.FindElement(By.Name("marketOrderCheck")).GetAttribute("checked") != "true")
+            {
+                driver.FindElement(By.Name("marketOrderCheck")).Click();
+            }
+            driver.FindElement(By.Id("sellCost")).SendKeys((number * getCurrentSellPrice()).ToString());
+            System.Threading.Thread.Sleep(1000); //  Make sure the site's javascript updates
+            driver.FindElement(By.CssSelector("a[class='submitBtn button buttonLongest buttonBlue popup']")).Click();
+            return true;
+        }
+        private bool buyBitCoins(double number)
+        {
+            driver.Navigate().GoToUrl("http://mtgox.com/#buy");
+            if (driver.FindElement(By.Name("marketOrderCheck")).GetAttribute("checked") != "true")
+            {
+                driver.FindElement(By.Name("marketOrderCheck")).Click();
+            }
+            driver.FindElement(By.Id("buyCost")).SendKeys((number * getCurrentBuyPrice()).ToString());
+            System.Threading.Thread.Sleep(1000); //  Make sure the site's javascript updates
+            driver.FindElement(By.CssSelector("a[class='submitBtn button buttonLongest popup']")).Click();
+            return true;
+        }
+        private double getCurrentBuyPrice()
+        {
+
+                return Double.Parse(driver.FindElement(By.Id("buyPrice")).GetAttribute("value"));
+
+        }
+           private double getCurrentSellPrice()
+        {
+
+            return Double.Parse(driver.FindElement(By.Id("sellPrice")).GetAttribute("value"));
+            
+
+        }
+        private void loginToWebsite()
+        {
+            driver.Navigate().GoToUrl("http://mtgox.com/");
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(By.Id("username")).SendKeys("mrblotchkins");
+            loadPassword(driver);
+            driver.FindElement(By.Name("LOGIN")).SendKeys(OpenQA.Selenium.Keys.Down + OpenQA.Selenium.Keys.Enter);
+            System.Threading.Thread.Sleep(1000);
+        }
+        private void loadPassword(IWebDriver driver)
+        {
+            driver.FindElement(By.Id("password")).SendKeys("Emilycat5589");
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loginToWebsite();
+            lbl_Price.Text = "Test";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Double d = getCurrentBuyPrice();
+            System.Windows.Forms.MessageBox.Show(d.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sellBitcoins(Double.Parse(textBox1.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            buyBitCoins(Double.Parse(textBox2.Text));
+        }
+
+
+        private void SetLabelText(String text)
+        {
+            lbl_Price.Text = text;
+        }
+        private void SetIntervalLabelText(Double minutes, bool up)
+        {
+            lbl_Interval.Text = minutes.ToString() + "Minutes\n" + up.ToString();
+        }
+        private Double determineMinSellPrice(Double numOfBitcoins, Double buyPricePerBitcoin, Double desiredProfit, Double tradeFee)
+        { 
+           return ((desiredProfit / numOfBitcoins) + buyPricePerBitcoin)/(1 - 2*tradeFee + tradeFee*tradeFee); //  Equation to determine minimum required profit
+        }
+        private StockAction getMarketStatus(List<Double> priceHistory, int numberPerGroup)
+        { 
+            //  We will determine this by taking the most recent 5 data points, and comparing to the previous 5.
+            //  This may give some sort of indication of the state of the market
+            Double recent = 0;
+            for(int x = 1; x <= numberPerGroup; x++)
+            {
+                recent += priceHistory[priceHistory.Count - x];
+            }
+            recent = recent / (Double)numberPerGroup;
+
+            Double previous = 0;
+            for (int x = 1; x <= numberPerGroup; x++)
+            {
+                previous += priceHistory[priceHistory.Count - numberPerGroup - x];
+            }
+            previous = previous / (Double)numberPerGroup;
+
+            if (recent > previous) 
+            {
+                return  StockAction.Up;
+            }
+            else if (recent == previous)
+            {
+                return StockAction.Steady;
+            }
+            else
+            {
+                return StockAction.Down;
+            }
+        }
+        private void showCurrentPrice()
+        {
+            Double minAmount = Double.Parse(textBox3.Text);
+            Double currentPrice = getCurrentBuyPrice();
+            SetLabelText_Del labelDel = SetLabelText;
+            SetIntervalLabelText_Del intervalLabelDel = SetIntervalLabelText;
+            List<Double> priceHistory = new List<double>();
+            Double totalTime = 0;
+            Double initialPrice = currentPrice;
+            while (currentPrice < minAmount)
+            {
+                object[] obj = new object[1];
+                obj[0] = "$" + currentPrice.ToString();
+                lbl_Price.Invoke(labelDel, obj);
+                System.Threading.Thread.Sleep(millisecondsPerDataPoint);
+                currentPrice = getCurrentBuyPrice();
+                totalTime += millisecondsPerDataPoint;
+                priceHistory.Add(currentPrice);
+                if (Math.Abs(currentPrice - initialPrice) >= 3)
+                {
+                    obj = new object[2];
+                    obj[0] = (totalTime / (Double)1000) / (Double)60; // Get number of minutes for interval
+                    if (currentPrice > initialPrice) //  Determine if interval was +/-
+                    {
+                        obj[1] = true;
+                    }
+                    else
+                    {
+                        obj[1] = false;
+                    }
+                    lbl_Interval.Invoke(intervalLabelDel, obj);
+                    totalTime = 0;
+                    initialPrice = currentPrice;
+                }
+            }
+            System.Windows.Forms.MessageBox.Show(getCurrentBuyPrice().ToString());
+        }
+        private Double getMarketAverage(List<Double> priceHistory, int dataPointsPerPeriod, int numberOfPeriodsToAverage)
+        {
+            Double periodAverage = 0;
+            for (int i = 0; i < numberOfPeriodsToAverage; i++)
+            {
+                Double dataPointAverage = 0;
+                for (int j = 1; j <= dataPointsPerPeriod; j++)
+                {
+                    dataPointAverage += priceHistory[priceHistory.Count - i*dataPointsPerPeriod - j];
+                }
+                dataPointAverage = dataPointAverage / (Double)dataPointsPerPeriod;
+                periodAverage += dataPointAverage;
+            }
+            periodAverage = periodAverage / (Double)numberOfPeriodsToAverage;
+            return periodAverage;
+        }
+        private void runSystem()
+        {
+            loginToWebsite();
+
+            //  Variables to hold the market history
+            List<Double> priceHistory = new List<double>();
+            
+            //  Capture 6 initial periods
+            for (int i = 0; i < (6 * dataPointsPerPeriod); i++)
+            {
+                System.Threading.Thread.Sleep(millisecondsPerDataPoint);
+                priceHistory.Add(getCurrentBuyPrice());
+                
+            }
+
+            while (1 == 1)
+            {
+                //  Loop until the market goes up
+                while (!((getMarketStatus(priceHistory, dataPointsPerPeriod) == StockAction.Up) & (getCurrentBuyPrice() <= getMarketAverage(priceHistory, dataPointsPerPeriod, 6))))
+                {
+                    //  Continue capturing data points
+                    for (int i = 0; i < dataPointsPerPeriod; i++)
+                    {
+                        System.Threading.Thread.Sleep(millisecondsPerDataPoint);
+                        priceHistory.Add(getCurrentBuyPrice());
+                        
+                    }
+                }
+
+                //  Since the market is going up, and the price is equal or below the average, buy some bitcoins! Also start the timer
+                Double MillisecondTimer = 0;
+                Double initialPrice = getCurrentBuyPrice();
+                Double currentPrice = initialPrice;
+                try
+                {
+                    buyBitCoins(numberOfBitcoins);
+                }
+                catch (Exception ex)
+                {
+                    loginToWebsite();
+                    buyBitCoins(numberOfBitcoins);
+                }
+                //  Keep capturing points until the market does down
+                while (!(getMarketStatus(priceHistory, dataPointsPerPeriod) == StockAction.Down))
+                {
+                    for (int i = 0; i < dataPointsPerPeriod; i++)
+                    {
+                        System.Threading.Thread.Sleep(millisecondsPerDataPoint);
+                        priceHistory.Add(getCurrentSellPrice());
+                        MillisecondTimer += millisecondsPerDataPoint;
+                        
+                    }
+               }
+
+                //  Now that the market's shown signs of declining, just wait until the price is at least at the minimum to profit
+                //  ... Probably a better algorithm for this =)
+                currentPrice = priceHistory[priceHistory.Count - 1]; // Get most recent price
+                while (currentPrice < determineMinSellPrice(numberOfBitcoins, initialPrice, minimumProfit, tradeFee))
+                {
+                    System.Threading.Thread.Sleep(millisecondsPerDataPoint);
+                    currentPrice = getCurrentSellPrice();
+                    priceHistory.Add(currentPrice);
+                    MillisecondTimer += millisecondsPerDataPoint;
+                }
+
+
+                try
+                {
+                    sellBitcoins(getBitcoinCountAfterFee(numberOfBitcoins, tradeFee));
+                }
+                catch (Exception ex)
+                {
+                    loginToWebsite();
+                    sellBitcoins(getBitcoinCountAfterFee(numberOfBitcoins, tradeFee));
+                }
+                Double boughtPrice = getActualBuyMoneySpent(initialPrice, numberOfBitcoins);
+                Double sellPrice = getActualSellMoneyEarned(currentPrice, numberOfBitcoins, tradeFee);
+                writeTextToFile("Time Sold: " + DateTime.Now.ToString());
+                writeTextToFile("Elapsed Minutes: " + ((MillisecondTimer / 1000) / 60).ToString());
+                writeTextToFile("Profit: " + (sellPrice - boughtPrice).ToString());
+                writeTextToFile("Buy Price (Fee from BTC): " + boughtPrice.ToString());
+                writeTextToFile("Actual # of BTC Bought: " + getBitcoinCountAfterFee(numberOfBitcoins, tradeFee));
+                writeTextToFile("Sell Price: " + sellPrice.ToString());
+                writeTextToFile("");
+                exchangeData.Add(new ExchangeData(boughtPrice, sellPrice, (MillisecondTimer / 1000) / 60, sellPrice - boughtPrice)); //  TODO, make functions for real selling / buying prices / profit
+
+            }
+
+        }
+        private void showPlot()
+        {
+           // Chart c = new Chart();
+        }
+        private void writeTextToFile(String text)
+        {
+           StreamWriter sw = File.AppendText("D:/BTC_TradeHistory.txt");
+            sw.WriteLine(text);
+            sw.Close();
+        }
+        private Double getActualBuyMoneySpent(Double buyPricePerBitcoin, Double numberOfBitcoins)
+        {
+            return (numberOfBitcoins * buyPricePerBitcoin);
+        }
+
+        private Double getActualSellMoneyEarned(Double sellPricePerBitcoin, Double numberOfBitcoinsPurchased, Double tradeFee)
+        {
+            Double actualBitcoinsBought = getBitcoinCountAfterFee(numberOfBitcoinsPurchased, tradeFee);
+            return actualBitcoinsBought * sellPricePerBitcoin - actualBitcoinsBought * sellPricePerBitcoin * tradeFee;
+        }
+
+        private Double getBitcoinCountAfterFee(Double numberOfBitcoins, Double tradeFee)
+        {
+            return numberOfBitcoins - numberOfBitcoins * tradeFee;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = new ThreadStart(showCurrentPrice);
+            Thread th = new Thread(showCurrentPrice);
+            th.Start();
+
+        }
+        private void sendEmail()
+        {
+            string server = "smtp-sl.vtext.com";
+            string to = "4134547707@vtext.com";
+            string from = "4134547707@vtext.com";
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = "Bitcoin Alert!";
+            message.Body = "The price of bitcoins has hit " + getCurrentBuyPrice().ToString();
+            SmtpClient client = new SmtpClient(server);
+            client.UseDefaultCredentials = true;
+            client.Send(message);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            sendEmail();
+        }
+        private enum StockAction
+        { 
+           Up, Down, Steady
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            runSystem();
+        }
+
+        private struct ExchangeData
+        {
+            public Double BuyPrice;
+            public Double SellPrice;
+            public Double ElapsedMinutes;
+            public Double Profit;
+
+            public ExchangeData(Double buyPrice, Double sellPrice, Double elapsedMinutes, Double profit)
+            {
+                this.BuyPrice = buyPrice;
+                this.SellPrice = sellPrice;
+                this.ElapsedMinutes = elapsedMinutes;
+                this.Profit = profit;
+            }
+        }
+    }
+}
